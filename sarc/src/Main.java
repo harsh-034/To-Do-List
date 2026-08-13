c46126f0999bd9abeaeb30a88d8292c2a8d270e2
harsh-034 <harshgupta00034@gmail.com>
2026-08-13 16:38:47 +0530

    Update To do List

--- sarc/src/Main.java
+++ sarc/src/Main.java
@@ -1,76 +1,28 @@
+import java.lang.runtime.SwitchBootstraps;
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Iterator;
-
-
-
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> task = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
-        int choice;
-        do{
-            System.out.println("-------------To Do List------------");
-            System.out.println("1. Add");
-            System.out.println("2. View");
-            System.out.println("3. delete");
-            System.out.println("4. Edit");
-            System.out.println("5. Exit");
-
-            System.out.println("Enter any number :");
-            choice = sc.nextInt();
-            switch(choice) {
-                case 1:
-                    System.out.println("Enter Your Task :");
-                    sc.nextLine();
-                    String addtask = sc.nextLine();
-                    task.add(addtask);
-                    System.out.println("New Task add SuccessFul....");
-                    break;
-                case 2:
-                    if (task.size() == 0) {
-                        System.out.println("Empty Task...............");
-                        System.out.println("Please add new Task .............");
+        Scanner input = new Scanner(System.in);
+       int choice;
+        do {
+            Task.Display();
+            choice = input.nextInt();
+            switch(choice){
+                case 1: Task.add();
+                       break;
+                case 2: Task.view();
+                       break;
+                case 3: Task.edit();
                         break;
-                    }else {
-                        System.out.println("  Your Task List ");
-                        for (int i = 0; i < task.size(); i++) {
-                            System.out.println(i + 1 + ". " + task.get(i));
-                        }
+                case 4: Task.delete();
                         break;
-                    }
-                case 3:
+                default:
+                    System.out.println("Plz Enter right number......");
 
-                    System.out.print("please Enter Task number for remove :");
-                    int n = sc.nextInt();
-                    if (task.size() > n-1) {
-                        task.remove(n - 1);
-                        System.out.println("Remove "+n+" Task Successfull ..............");
-                    }else{
-                        System.out.println("Plz Enter Valid Task Number........ ");
-                    }
-                     break;
-                case 4:
-                    System.out.println("Which task number update :");
-                    int n1 = sc.nextInt();
-                    if(task.size()>n1-1) {
-                        System.out.println("Write your update task :");
-                        sc.nextLine();
-                        String newTask = sc.nextLine();
-                        task.set(n1 - 1, newTask);
-                        System.out.println("Update Successful...........");
-                    }else{
-                        System.out.println("Plz Enter Valid Task Number........ ");
-                    }
-                    break;
-                default:if(choice != 5){
-                    System.out.println("Plese enter valid Number........... ");
-
-                }
             }
-
         }while(choice != 5);
+
     }
     }
 
--- sarc/src/Task.java
+++ sarc/src/Task.java
@@ -4,26 +4,56 @@ import java.util.Scanner;
 public class Task {
     static ArrayList<String> task = new ArrayList<>();
     public static Scanner sc = new Scanner(System.in);
+
+    // Display
     public static void Display(){
+        System.out.println("<-------------Start To Do List ------------->");
         System.out.println("1. Add new Task.");
         System.out.println("2. View all Task.");
         System.out.println("3. Edit Task. ");
         System.out.println("4. Delete your Task. ");
         System.out.println("5. Exit");
-        System.out.println("Enter Tour Choice  Number : ");
+        System.out.println("Enter Choice  Number : ");
     }
-    public static void Add(){
+
+    //Add Your Task
+    public static void add(){
         System.out.println("Enter your task : ");
          String taskAdd = sc.nextLine();
          task.add(taskAdd);
         System.out.println("Success full add your Task....");
     }
-    public static void View(){
-        System.out.println("<--------List of Task---------->");
-        for (int i = 0; i<task.size();i++)
-            System.out.println(task.get(i+1));
 
+    //View Your All Task
+    public static void view(){
+        if(task.isEmpty()){
+            System.out.println("Your Task is Empty.....?");
+            System.out.println("PLz Add the Task..... ");
 
+        }else {
+            System.out.println("<--------List of Task---------->");
+            for (int i = 0; i < task.size(); i++) {
+                System.out.println(i + 1 + " | " + task.get(i));
+            }
+        }
+    }
+
+    //Edit Your Task
+    public static void edit(){
+        System.out.println("Enter task number for update. ");
+        int update = sc.nextInt();
+        System.out.println("Write Your update task.");
+        String updateTask = sc.nextLine();
+        task.set(update,updateTask);
+        System.out.println("Successful update your task.");
+    }
+
+    //Delete Your Task.
+    public static void delete(){
+        System.out.println("Enter your task number for Delete.");
+        int delete = sc.nextInt();
+        task.remove(delete);
+        System.out.println("Successful remove your task...");
     }
 
 }
